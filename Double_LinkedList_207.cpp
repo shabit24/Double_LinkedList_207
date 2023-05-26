a8db0d45c916d1e5a24b0793a8c7efbcb1f042b2
shabit24 <hossain.shabit24@gmail.com>
2023-05-26 15:06:16 +0700

    traverse function

--- Double_LinkedList_207.cpp
+++ Double_LinkedList_207.cpp
@@ -102,3 +102,20 @@ bool DoubleLinkedList::deleteNode(int rollNo) {
 	delete current;
 	return true;
 }
+bool DoubleLinkedList::listEmpty() {
+	return (START == NULL);
+
+}
+void DoubleLinkedList::traverse() {
+	if (listEmpty())
+		cout << "\nlist is empty" << endl;
+	else {
+		cout << "\nRecord in ascnding order of roll number are:" << endl;
+		Node* currentNode = START;
+		while (currentNode != NULL) {
+			cout << currentNode->noMhs << " " << currentNode->name << endl;
+			currentNode = currentNode->next;
+
+		}
+	}
+}
