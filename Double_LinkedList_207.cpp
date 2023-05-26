277ddf21079544c9ee480aa4e98b95d0d51d44b4
shabit24 <hossain.shabit24@gmail.com>
2023-05-26 11:15:20 +0700

    insert a node

--- Double_LinkedList_207.cpp
+++ Double_LinkedList_207.cpp
@@ -26,3 +26,29 @@ public:
 	void searchData();
 
 };
+
+DoubleLinkedList::DoubleLinkedList() {
+	START = NULL;
+}
+
+
+void DoubleLinkedList::addNode() {
+	int nim;
+	string nm;
+	cout << "\nEnter the roll number of the student: ";
+	cin >> nim;
+	cout << "\Enter the name of student: ";
+	cin >> nm;
+	Node* newNode = new Node(); //step 1
+	newNode->noMhs = nim;
+	newNode->name = nm;
+	if (START == NULL || nim <= START->noMhs) {
+		if (START != NULL && nim == START->noMhs) {
+			cout << "\nDuplicate number not allowed" << endl;
+			return;
+		}
+		newNode->next = START;
+	}
+		
+
+}
