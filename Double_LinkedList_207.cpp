27d921a824d0ff44ab135a55a2185dd551aac190
shabit24 <hossain.shabit24@gmail.com>
2023-05-26 11:29:40 +0700

    step 7

--- Double_LinkedList_207.cpp
+++ Double_LinkedList_207.cpp
@@ -69,5 +69,9 @@ void DoubleLinkedList::addNode() {
 		return;
  }
 	newNode->next = current->next;
+	newNode->prev = current;
+	if (current->next != NULL)
+		current->next->prev = newNode;
+	current->next = newNode;
 
 }
