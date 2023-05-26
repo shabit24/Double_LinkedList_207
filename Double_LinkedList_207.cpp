3cd8ad780777fef24a83612e9e306e9177da8936
shabit24 <hossain.shabit24@gmail.com>
2023-05-26 15:52:10 +0700

    switch

--- Double_LinkedList_207.cpp
+++ Double_LinkedList_207.cpp
@@ -119,3 +119,65 @@ void DoubleLinkedList::traverse() {
 		}
 	}
 }
+
+void DoubleLinkedList::revtraverse() {
+	if (listEmpty()) {
+		cout << "\nList is Empty" << endl;
+	}
+	else {
+		cout << "\nRecords in descending order of roll number are:" << endl;
+		Node* currentNode = START;
+		while (currentNode->next != NULL)
+			currentNode = currentNode->next;
+		while (currentNode != NULL) {
+			cout << currentNode->noMhs << " " << currentNode->name << endl;
+			currentNode = currentNode->prev;
+
+		}
+	}
+}
+
+void DoubleLinkedList::hapus() {
+	if (listEmpty() == true) {
+		cout << "\nList is empty" << endl;
+
+	}
+	Node* prev, * curr;
+	prev = curr = NULL;
+	cout << "\nEnter roll number of student whose record you want to secach: ";
+	int num;
+	cin >> num;
+	if (DoubleLinkedList::search(num, &prev, &curr) == false)
+		cout << "\nRecord not found" << endl;
+	else {
+		cout << "\nRecord found" << endl;
+		cout << "\nRoll number: " << curr->noMhs << endl;
+		cout << "\nName: " << curr->name << endl;
+	}
+}
+
+
+int main() {
+	DoubleLinkedList obj;
+	while (true) {
+		try {
+			cout << "\nMenu" << endl;
+			cout << "\nMenu" << endl;
+			cout << "1. Add a record to the list" << endl;
+			cout << "2. Delete a record from list" << endl;
+			cout << "3. View all records on ascending order of roll numbers" << endl;
+			cout << "4. View all records on descending order of roll numbers" << endl;
+			cout << "5. Search for a record inthe list" << endl;
+			cout << "6. Exit" << endl;
+			cout << "\nEnter your choice(1-6): ";
+			char ch;
+			cin >> ch;
+			switch (ch) {
+
+			}
+
+
+
+		}
+	}
+}
