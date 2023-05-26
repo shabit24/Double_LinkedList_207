6935aa7a230dbc2c60425d4bda9a2bdc75937e33
shabit24 <hossain.shabit24@gmail.com>
2023-05-26 11:00:45 +0700

    Add project files.

---
+++ Double_LinkedList_207.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Node {
+public:
+	int noMhs;
+	string name;
+	Node* next;
+	Node* prev;
+
+};
+
+class DoubleLinkedList {
+private:
+	Node* START;
+public:
+	DoubleLinkedList();
+	void addNode();
+	bool search(int rollNo, Node* previous, Node* current);
+	bool deleteNode(int rollNo);
+	bool listEmpty();
+	void traverse();
+	void hapus();
+	void searchData();
+
+};
