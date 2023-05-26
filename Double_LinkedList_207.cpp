ddf8d3693599ebdd5dd46666df3f465d723341bf
shabit24 <hossain.shabit24@gmail.com>
2023-05-26 15:58:38 +0700

    catch

--- Double_LinkedList_207.cpp
+++ Double_LinkedList_207.cpp
@@ -173,11 +173,33 @@ int main() {
 			char ch;
 			cin >> ch;
 			switch (ch) {
-
+			case '1':
+				obj.addNode();
+				break;
+			case '2':
+				obj.hapus();
+				break;
+			case '3':
+				obj.traverse();
+				break;
+			case '4':
+				obj.revtraverse();
+				break;
+			case '5':
+				obj.searchData();
+				break;
+			case '6':
+				return 0;
+			default:
+				cout << "\nInvalid option" << endl;
+				break;
 			}
 
 
 
+		}
+		catch (exception& e) {
+			cout << "Check for the values entered" << endl;
 		}
 	}
 }
